670ffe00bc4fda9727fe99de0921d7ea6d9340fd
rtib <rtib@users.noreply.github.com>
2016-01-06 22:27:36 +0100

    spec tests for dhcp config added

---
+++ spec/defines/dhcp_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'debnet::iface::dhcp' do
+  let(:title) { 'eth0' }
+  let(:facts) {{
+    :concat_basedir => '/var/lib/puppet/concat',
+  }}
+
+  context 'eth0 standard' do
+    it {
+      should contain_package('isc-dhcp-client')
+      should contain_debnet__iface('eth0').with({
+        'method' => 'dhcp',
+        'family' => 'inet',
+      })
+    }
+  end
+
+  context 'eth0 with params' do
+    let(:params) {{
+      :hostname  => 'test',
+      :metric    => '20',
+      :leasetime => '1234',
+      :vendor    => 'vendor',
+      :client    => 'client',
+      :hwaddress => '11:22:33:44:55:66',
+    }}
+    it {
+      should contain_package('isc-dhcp-client')
+      should contain_debnet__iface('eth0').with({
+        'method'    => 'dhcp',
+        'family'    => 'inet',
+        'hostname'  => 'test',
+        'metric'    => '20',
+        'leasetime' => '1234',
+        'vendor'    => 'vendor',
+        'client'    => 'client',
+        'hwaddress' => '11:22:33:44:55:66',
+      })
+    }
+  end
+end
