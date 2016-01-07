b875bb4ae18c2e0921b34eaa122829d5ce34dbc9
rtib <rtib@users.noreply.github.com>
2016-01-07 22:53:31 +0100

    spec for debnet::iface::static added

---
+++ spec/defines/static_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'debnet::iface::static' do
+  let(:title) { 'eth0' }
+  let(:facts) {{
+    :concat_basedir => '/var/lib/puppet/concat',
+  }}
+
+  context 'eth0 with address and mask' do
+    let(:params) {{
+      :address  => '192.168.254.253',
+      :netmask  => '24',
+    }}
+    it {
+      should contain_debnet__iface('eth0').with({
+        'method'  => 'static',
+        'family'  => 'inet',
+        'address' => '192.168.254.253',
+        'netmask' => '24',
+      })
+    }
+  end
+end
